97c78e5173623f987d8d8501d899798560159598
grh <297909804@qq.com>
2020-02-11 22:17:27 +0800

    github authorize

---
+++ src/main/java/com/ncu/community/controller/AuthorizeController.java
@@ -0,0 +1,30 @@
+package com.ncu.community.controller;
+
+import com.ncu.community.dto.AccessTokenDTO;
+import com.ncu.community.dto.GithubUser;
+import com.ncu.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("2b0a53bc531a21c9f53b");
+        accessTokenDTO.setClient_secret("38d086f101e856987f52635d0412472cec57cc1d");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        return "index";
+    }
+}
